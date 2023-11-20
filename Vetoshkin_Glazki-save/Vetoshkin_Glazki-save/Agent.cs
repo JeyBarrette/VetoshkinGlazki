e84f408ba0aaaec1204e6dd394d37c4e1ef5f228
JeyBarrette <olegpetrow2001@gmail.com>
2023-11-20 16:03:25 +0500

    lab14 (не готова)

--- Vetoshkin_Glazki-save/Vetoshkin_Glazki-save/Agent.cs
+++ Vetoshkin_Glazki-save/Vetoshkin_Glazki-save/Agent.cs
@@ -11,7 +11,9 @@ namespace Vetoshkin_Glazki_save
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.Linq;
+    using System.Windows.Automation.Peers;
+
     public partial class Agent
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -33,7 +35,7 @@ namespace Vetoshkin_Glazki_save
         public string DirectorName { get; set; }
         public string INN { get; set; }
         public string KPP { get; set; }
-    
+
         public virtual AgentType AgentType { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<AgentPriorityHistory> AgentPriorityHistory { get; set; }
@@ -48,6 +50,20 @@ namespace Vetoshkin_Glazki_save
             {
                 return AgentType.Title;
             }
+        }
+
+        public int ProdCount => GetAllProducts();
+
+        public int GetAllProducts()
+        {
+            int count = 0;
+            var context = Vetoshkin_GlazkiEntities.GetContext().ProductSale.Where(p => p.AgentID == ID).ToList();
+            foreach (var productSale in context)
+            {
+                count = productSale.ProductCount;
+            }
+
+            return count;
         }
     }
 }
